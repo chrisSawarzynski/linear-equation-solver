3564667b3707611cc8eec210bc747a94b07afd76
Wojciech Regulski <wojtekreg@gmail.com>
2017-01-21 14:18:40 +0100

    initial

---
+++ mult_rownania/Matrix.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mult_rownania
+{
+    class Matrix
+    {
+        public float[,] matrix;
+
+        public Matrix(float[,] arr)
+        {
+            matrix = arr;
+        }
+
+        public void showMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("[" + (i + 1) + "," + (j + 1) + "]= ");
+                    Console.WriteLine(matrix[i, j]);
+                }
+            }
+        }
+
+        public void swap_rows(int r_base, int r_calc)
+        {
+            float temp;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                temp = matrix[r_base, i];
+                matrix[r_base, i] = matrix[r_calc, i];
+                matrix[r_calc, i] = temp;
+            }
+        }
+
+        public void first_to_one(int r_base)
+        {
+            float div = matrix[r_base, r_base];
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[r_base, i] = matrix[r_base, i] / div;
+            }
+        }
+
+        public void calculate(int r_base, int r_calc )
+        {
+            float calc_base = matrix[r_calc, r_base];
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[r_calc,i] = matrix[r_calc,i] - calc_base * matrix[r_base,i];
+            }
+        }
+
+        public void showResult()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.WriteLine("x" + (i + 1) + "= " + matrix[i, matrix.GetLength(0)]);
+            }
+        }
+     }
+}
---
+++ mult_rownania/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mult_rownania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            
+            n = int.Parse(Console.ReadLine());
+
+            float[,] array = new float[n, n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n+1; j++)
+                {
+                    Console.Write("["+(i+1)+","+(j+1)+"]= ");
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Matrix matrix = new Matrix(array);
+
+            //matrix.showMatrix();
+
+            for (int i = 0; i < n; i++)
+            {
+                if(matrix.matrix[i,i]==0)
+                {
+                    for (int j = i+1; j < n; j++)
+                    {
+                        if (matrix.matrix[j, i] != 0)
+                            matrix.swap_rows(i, j);
+                    }
+                }
+                    
+                matrix.first_to_one(i);
+                for (int j = 0; j < n; j++)
+                {
+                    if(i!=j)
+                    {
+                        //(new Thread(() => { matrix.calculate(i, j); })).Start();
+                        //Mutex.WaitAll();
+                        matrix.calculate(i, j);
+                    }
+                }
+            }
+
+            /*matrix.first_to_one(0);
+
+            matrix.showMatrix();
+
+            matrix.calculate(0, 1);
+
+            matrix.showMatrix();
+
+            matrix.first_to_one(1);
+
+            matrix.showMatrix();
+
+            matrix.calculate(1, 0);*/
+
+            //matrix.showMatrix();
+
+            matrix.showResult();
+
+            Console.ReadLine();
+        }
+    }
+}
