a448c403b35dea811717d6a6f1ef784ab8185b70
Wojciech Regulski <wojtekreg@gmail.com>
2017-01-23 20:07:00 +0100

    working_threads

--- mult_rownania/Program.cs
+++ mult_rownania/Program.cs
@@ -12,16 +12,27 @@ namespace mult_rownania
         static void Main(string[] args)
         {
             int n;
-            
+
+            Random random = new Random();
+            List<Task> tasks = new List<Task>();
+
+            Console.Write("Podaj liczbę niewiadomych: ");
             n = int.Parse(Console.ReadLine());
 
             float[,] array = new float[n, n + 1];
+
             for (int i = 0; i < n; i++)
             {
+                //Console.WriteLine((i + 1) + ". równanie:");
                 for (int j = 0; j < n+1; j++)
                 {
-                    Console.Write("["+(i+1)+","+(j+1)+"]= ");
-                    array[i, j] = int.Parse(Console.ReadLine());
+                    //Console.Write("["+(i+1)+","+(j+1)+"]= ");
+                    /*if (j != n)
+                        Console.Write("Współczynnik przy x" + (j + 1) + "= ");
+                    else
+                        Console.Write("Wyraz wolny= ");
+                    array[i, j] = int.Parse(Console.ReadLine());*/
+                    array[i, j] = random.Next(0,100);
                 }
             }
 
@@ -45,32 +56,24 @@ namespace mult_rownania
                 {
                     if(i!=j)
                     {
-                        //(new Thread(() => { matrix.calculate(i, j); })).Start();
-                        //Mutex.WaitAll();
-                        matrix.calculate(i, j);
+                        int index_i = i;
+                        int index_j = j;
+                        tasks.Add( Task.Factory.StartNew(
+                            () =>
+                            {
+                                matrix.calculate(index_i, index_j);
+                            }));
+                        //matrix.calculate(i, j);
                     }
                 }
+                Task.WaitAll(tasks.ToArray());
             }
-
-            /*matrix.first_to_one(0);
-
-            matrix.showMatrix();
-
-            matrix.calculate(0, 1);
-
-            matrix.showMatrix();
-
-            matrix.first_to_one(1);
-
-            matrix.showMatrix();
-
-            matrix.calculate(1, 0);*/
-
-            //matrix.showMatrix();
-
+            
+            Console.WriteLine();
             matrix.showResult();
 
             Console.ReadLine();
+
         }
     }
 }
